29589df14aa8d566bebd2f85f281a3d72ccf9d05
Olasupo <alfredolasupo@gmail.com>
2024-03-26 02:52:31 +0000

    CreateUser handler Refactored

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
@@ -15,7 +16,26 @@ type DB struct {
 
 //CreateUserhandler handles requests to create new user
 
-func CreateUserhandler(w http.ResponseWriter, r *http.Request) {
+func (db *DB) CreateUserhandler(w http.ResponseWriter, r *http.Request) {
 	var newUser models.User
-	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+		http.Error(w, "Invalid Request Body", http.StatusBadRequest)
+		return
+	}
+
+	// Insert the new user into the database
+	result, err := db.collection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	// Respond with success message
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{
+		"message":     "User created successfully",
+		"inserted_id": result.InsertedID,
+	}
+	json.NewEncoder(w).Encode(response)
 }
